1807eeadf56a4c896d5d36ff3eb7b475ec390b95
CHRISTIAN MCINTOSH_CLARKE <christian.mcintoshclarke@mail.utoronto.ca>
2023-09-18 18:44:23 -0400

    added nav bar, displayed my name and current time

--- flasky/hello.py
+++ flasky/hello.py
@@ -1,11 +1,22 @@
+from datetime import datetime
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+bootstrap = Bootstrap(app)
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/')
+def user():
+    day = datetime.today().strftime('%A') 
+    month = datetime.now().strftime('%B')
+    date = datetime.now().day
+    year = datetime.now().year
+    time = datetime.now().strftime('%I:%M %p')
+    timestamp = f"{day}, {month} {date}, {year} {time}"
+
+    return render_template('user.html', name='Christian', timestamp=timestamp)
